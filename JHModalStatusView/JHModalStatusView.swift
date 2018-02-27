b0f82b8faf1f6966c65e70c6bd37a40184238d38
Jahid Hasan Polash <jahidhasanpolash@gmail.com>
2018-02-27 23:50:28 +0600

    done copying

--- JHModalStatusView/JHModalStatusView.swift
+++ JHModalStatusView/JHModalStatusView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class JHModalStatusView: UIView {
+public class JHModalStatusView: UIView {
 
     @IBOutlet private weak var statusImage: UIImageView!
     @IBOutlet private weak var headingLabel: UILabel!
@@ -18,13 +18,31 @@ class JHModalStatusView: UIView {
     var contentView: UIView!
     public override init(frame: CGRect) {
         super.init(frame: frame)
-        setupView()
+        setupView()s
     }
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupView()
     }
     private func setupView(){
-        
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: self.nibName, bundle: bundle)
+        self.contentView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        addSubview(contentView)
+        contentView.center = self.center
+        contentView.autoresizingMask = []
+        contentView.translatesAutoresizingMaskIntoConstraints = true
+        headingLabel.text = ""
+        subheadingLabel.text = ""
+    }
+    // Provide functions to update view
+    public func set(image: UIImage) {
+        self.statusImage.image = image
+    }
+    public func set(headline text: String) {
+        self.headingLabel.text = text
+    }
+    public func set(subheading text: String) {
+        self.subheadingLabel.text = text
     }
 }
